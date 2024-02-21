96529421df3a1d506e902ea0b9aaccb32525df97
Ashmit <lci2021013@gmail.com>
2024-02-22 01:30:31 +0530

    delete user in show users page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,4 +8,5 @@ router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/signout',signout)
 router.get('/getusers',verifyToken,getusers)
+
 export default router
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -49,7 +49,22 @@ export default function DashUsers() {
     }
   }
   const handleDeleteUser = async()=>{
-
+      try{
+        const res= await fetch(`/api/user/delete/${userIdToDelete}`,{
+          method: 'DELETE'
+        })
+        const data = res.json()
+        if(res.ok){
+          setUsers((prev)=>prev.filter((user)=>user._id!==userIdToDelete))
+          setShowModal(false)
+        }
+        else{
+          console.log(data.message);
+        }
+      }
+      catch(error){
+        console.log(error.message)
+      }
   }
   
 
