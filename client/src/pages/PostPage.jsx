bd12b7867233082e2a283bafd992c702bb55a745
Ashmit <lci2021013@gmail.com>
2024-02-22 18:27:56 +0530

    add comment section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CommentSection from '../components/CommentSection'
 
 export default function PostPage() {
 
@@ -50,6 +51,7 @@ if(loading)return(
         <span className='italic'>{post && parseInt((post.content.length/1000).toFixed(0) + 1)} mins read</span>
      </div>
      <div className= 'p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}></div>
+     <CommentSection postId={post._id}/>
     </main>
   )
 }
