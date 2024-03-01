5b298715504493b520dd9a3598fdca490f547814
Ashmit <lci2021013@gmail.com>
2024-03-01 17:18:26 +0530

    like functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,14 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment'
 export default function CommentSection({postId}) {
   const {currentUser} = useSelector((state)=>state.user)
   const [comment,setComment] = useState('')
   const [commentError, setCommentError] = useState(null)
   const [comments, setComments] = useState([])
+  const navigate = useNavigate()
   const handleSubmit = async(e)=>{
     e.preventDefault()
     if(comment.length>200){
@@ -52,6 +53,31 @@ export default function CommentSection({postId}) {
      }
      getComments()
   }, [postId])
+  const handleLike = async(commentId) =>{
+     try{
+       if(!currentUser){
+        navigate('/sign-in')
+        return 
+       }
+       const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT'
+       })
+       if(res.ok){
+        const data = await res.json()
+        setComments(comments.map((comment)=>
+            comment._id===commentId?{
+                ...comment,
+                likes: data.likes,
+                numberOfLikes: data.numberOfLikes
+
+            } : comment
+        ))
+       }
+     }
+     catch(error){
+       console.log(error.message)
+     }
+  }
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ?
@@ -79,6 +105,7 @@ export default function CommentSection({postId}) {
                   maxLength='200'
                   onChange={(e)=>setComment(e.target.value)}
                   value={comment}
+                  
                   />
                 <div className='flex justify-between items-center mt-3'>
                     <Button outline gradientDuoTone='purpleToBlue' type='submit'>
@@ -105,7 +132,7 @@ export default function CommentSection({postId}) {
                 </div>
             {
                 comments.map((comment)=>(
-                    <Comment key={comment._id} comment={comment}/>
+                    <Comment key={comment._id} comment={comment} onLike={handleLike}/>
                 ))
             }
             </>
